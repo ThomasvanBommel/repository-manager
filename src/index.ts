3c7974770eb0841dea2d8bfd17a70f2f917ac3b0
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-04-01 14:41:48 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * File: index.ts
+ * Created: Thursday April 1st 2021
+ * Author: Thomas vanBommel
+ * 
+ * Last Modified: Thursday April 1st 2021 2:40pm
+ * Modified By: Thomas vanBommel
+ * 
+ * CHANGELOG:
+ */
+
+console.log("hi");
